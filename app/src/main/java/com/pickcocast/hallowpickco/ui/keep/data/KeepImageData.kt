b79ea95ae12627cdbd63b5076e478515c826535e
bokdoll <winnerhj1109@naver.com>
2019-11-03 04:23:36 +0900

    메인 화면 카드 뷰 수정& 통신 설정

--- app/src/main/java/com/pickcocast/hallowpickco/ui/keep/data/KeepImageData.kt
+++
@@ -1,6 +0,0 @@
-package com.pickcocast.hallowpickco.ui.keep.data
-
-class KeepImageData (
-    var keep_img_idx: Int,
-    var keep_img: String
-)
---
+++ app/src/main/java/com/pickcocast/hallowpickco/util/NetworkService.kt
@@ -0,0 +1,12 @@
+package com.pickcocast.hallowpickco.util
+
+import com.pickcocast.model.GetKeepImageDataResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NetworkService {
+    // 3. 보관함
+    @GET("")
+    fun getKeepImageResponse(
+    ):Call<GetKeepImageDataResponse>
+}
